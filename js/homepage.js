9325d28b67812297effb0b28588c5716c6252b19
Dang Quyen <dangquyen@fgct.net>
2018-06-18 21:00:48 +0700

    product detail - carousel

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,35 @@
 (function($) {
     
     function runSlider(){
-         $('.flexslider').flexslider({
+         $('#main-slider').flexslider({
+            animation: "slide",
+            directionNav: false,
+            drag: true
+          });
+         $('#main-mobile-slider').flexslider({
+            animation: "slide",
+            directionNav: false,
+            drag: true
+          });
+        $('#product-detail-slider').flexslider({
+            animation: "slide",
+            animationLoop: true,
+            itemWidth: 210,
+            itemMargin: 5,
+            minItems: 2,
+            maxItems: 4,
+            move: 1,
+            slideshowSpeed: 1000,
+            controlNav: false,
+            pauseOnAction: false,
+            pauseOnHover: true
+          });
+        $('#product-list-slider').flexslider({
+            animation: "slide",
+            directionNav: false,
+            drag: true
+          });
+        $('.customer-reviews-slider').flexslider({
             animation: "slide",
             directionNav: false,
             drag: true
